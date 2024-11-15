6d567e2179e25affb5387b66ec839910a6ff6d1b
Dani5822 <75309763+Dani5822@users.noreply.github.com>
2024-11-15 16:30:18 +0100

    javitas

--- src/playlist/dto/update-playlist.dto.ts
+++ src/playlist/dto/update-playlist.dto.ts
@@ -1,7 +1,6 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreatePlaylistDto } from './create-playlist.dto';
-import { Song } from "@prisma/client"
+
 
 export class UpdatePlaylistDto extends PartialType(CreatePlaylistDto) {
-    Songs:[Song]
 }
